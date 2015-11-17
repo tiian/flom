8c9bd99082aaf5b4b8af34028fbdc13a2e39f231
Christian Ferrari <tiian@users.sourceforge.net>
2015-11-17 23:27:34 +0100

    Release 1.1.0 commit

---
+++ tests/scripts/skip_ipv6_multicast_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright (c) 2013-2015, Christian Ferrari <tiian@users.sourceforge.net>
+# All rights reserved.
+#
+# This file is part of FLOM.
+#
+# FLOM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# FLOM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FLOM.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# These Linux distributions do not pass the IPv6 multicast tests
+grep '^CentOS release 6' /etc/centos-release
+if test $? -eq 0
+then
+	exit 77
+else
+	exit 0
+fi
