f906508104cded71321030792636b412c4239808
Christian Ferrari <tiian@users.sourceforge.net>
2016-02-26 23:49:51 +0100

    Test cases for TLS --debug-feature completed

---
+++ tests/scripts/sleep_and_kill.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Copyright (c) 2013-2016, Christian Ferrari <tiian@users.sourceforge.net>
+# All rights reserved.
+#
+# This file is part of FLOM.
+#
+# FLOM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# FLOM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FLOM.  If not, see <http://www.gnu.org/licenses/>.
+#
+if test $# -lt 1
+then
+	echo "At least one parameter must be specified"
+	exit 1
+fi
+sleep $1
+pkill flom
+
--- tests/scripts/tls_setup.sh
+++ tests/scripts/tls_setup.sh
@@ -1,11 +1,72 @@
 #!/bin/sh
-echo "Creating certification authorities"
-mkdir -v CA1 CA2 || exit $?
-cp -v $ETCPATH/openssl.cnf CA1 || exit $?
-cp -v $ETCPATH/openssl.cnf CA2 || exit $?
-# entering first CA directory
-cd CA1 || exit $?
-mkdir certs crl newcerts private || exit $?
-echo "01" > serial || exit $?
-cp /dev/null index.txt || exit $?
-openssl req -new -x509 -keyout private/cakey.pem -out cacert.pem -days 365 -config openssl.cnf -subj "/C=IT/ST=TV/L=Mogliano Veneto/O=FLoM Software/OU=R and D/CN=FLoM CA 1" -passout pass:flomca1 || exit $?
+#
+# Copyright (c) 2013-2016, Christian Ferrari <tiian@users.sourceforge.net>
+# All rights reserved.
+#
+# This file is part of FLOM.
+#
+# FLOM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# FLOM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FLOM.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# retrieving unique id
+UNIQUE_ID=$(flom --unique-id)
+echo "FLoM unique IS is $UNIQUE_ID"
+echo "Creating certification authorities and certificates"
+
+create_ca () {
+	echo "Creating CA $CA"
+	mkdir -v ${CA} || exit $?
+	cp -v $ETCPATH/openssl.cnf ${CA} || exit $?
+	# entering first CA directory
+	cd $CA || exit $?
+	mkdir certs crl newcerts private || exit $?
+	echo "01" > serial || exit $?
+	cp /dev/null index.txt || exit $?
+	# creating CA certificate
+	openssl req -new -x509 -keyout private/cakey.pem -out cacert.pem -days 365 -config openssl.cnf -subj "/C=IT/ST=TV/L=Mogliano Veneto/O=FLoM Software/OU=R and D/CN=FLoM $CA" -passout pass:flom$CA || exit $?
+}
+
+create_cert() {
+	echo "Creating certificate for peer $PEER"
+	# creating peers' certificates
+	openssl req -nodes -new -x509 -keyout ${PEER}_${CA}_key.pem -out ${PEER}_${CA}_req.pem -days 365 -config openssl.cnf -subj "/C=IT/ST=TV/L=Mogliano Veneto/O=FLoM Software/OU=${PEER}/CN=${UNIQUE_ID}" || exit $?
+	openssl x509 -x509toreq -in ${PEER}_${CA}_req.pem -signkey ${PEER}_${CA}_key.pem -out tmp.pem || exit $?
+	openssl ca -batch -config openssl.cnf -policy policy_anything -passin pass:flom${CA} -out ${PEER}_${CA}_cert.pem -infiles tmp.pem || exit $?
+	rm tmp.pem || exit $?
+}
+
+CA=CA1
+create_ca || exit $?
+PEER=peer1
+create_cert || exit $?
+PEER=peer2
+create_cert || exit $?
+# wrong UNIQUE_ID
+TRUE_UNIQUE_ID=${UNIQUE_ID}
+UNIQUE_ID=00112233445566778899aabbccddeeff
+PEER=peer3
+create_cert || exit $?
+UNIQUE_ID=${UNIQUE_ID}
+
+cd ..
+
+CA=CA2
+create_ca || exit $?
+PEER=peer1
+create_cert || exit $?
+PEER=peer2
+create_cert || exit $?
+
+cd ..
+
+echo "Certification authorities and certificates completed!"
