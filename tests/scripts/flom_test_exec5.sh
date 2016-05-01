b0c62466ea1520213513c716d5c93316359e1672
Christian Ferrari <tiian@users.sourceforge.net>
2016-05-01 19:12:47 +0200

    Release 1.3.1 commit

---
+++ tests/scripts/flom_test_exec5.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# Copyright (c) 2013-2016, Christian Ferrari <tiian@users.sourceforge.net>
+# All rights reserved.
+#
+# This file is part of FLoM.
+#
+# FLoM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# FLoM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FLoM.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Options:
+# $1 ID
+# $2 delay time
+# $3 duration time
+# $4 exit code of the executed command (sleep_echo_exit)
+# $5 flom args
+
+if test $# -lt 5
+then
+	echo "At least five parameters must be specified"
+	exit 1
+fi	
+
+# Triple comments are used to distinguish debug rows
+
+# print start message
+###echo -n $(date +'%s %N')
+###echo " $1 starting and waiting $2 seconds"
+# wait some seconds...
+sleep $2 
+# execution with duration
+###echo -n $(date +'%s %N')
+echo " $1 locking for $3 seconds"
+###echo "flom $4 -- sleep $3"
+flom $5 -- sleep_echo_exit.sh $3 $4
+EXIT_RC=$?
+# print end message
+###echo -n $(date +'%s %N')
+echo " $1 ending"
+exit $EXIT_RC
