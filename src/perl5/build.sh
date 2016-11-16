51a15473c17a4a306817844b7991f03d99680862
Kevin L. Esteb <kevin@kesteb.us>
2016-11-15 16:00:30 -0800

    simplified the sed commands

--- src/perl5/build.sh
+++ src/perl5/build.sh
@@ -11,10 +11,7 @@ swig -perl5 -const -I.. flom.i
 # flatten the namespace - make it more perlish
 #
 sed -i 's/*flom_/*/g' Flom.pm
-sed -i 's/sub FLOM_RC/sub RC/g' Flom.pm
-sed -i 's/sub FLOM_ES/sub ES/g' Flom.pm
-sed -i 's/sub FLOM_LOCK/sub LOCK/g' Flom.pm
-sed -i 's/sub FLOM_HANDLE/sub HANDLE/g' Flom.pm
+sed -i 's/sub FLOM_/sub /g' Flom.pm
 #
 # let perl create the makefile
 #
