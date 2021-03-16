417feadcc909b7541d9dbca1a4c6707185cd6be3
Christian Ferrari <tiian@users.sourceforge.net>
2021-03-16 21:30:47 +0100

    Updated copyright

--- tests/scripts/flom_test_exec2.sh
+++ tests/scripts/flom_test_exec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2013-2020, Christian Ferrari <tiian@users.sourceforge.net>
+# Copyright (c) 2013-2021, Christian Ferrari <tiian@users.sourceforge.net>
 # All rights reserved.
 #
 # This file is part of FLOM.
